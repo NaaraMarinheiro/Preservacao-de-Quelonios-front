fa5b866ad94942decdae7c99e6fe29bbf2adfc77
YacciRocha <yaccirs@gmail.com>
2022-07-10 01:14:29 -0300

    Insercao da estrutura HTML e CSS das paginas de formularios

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,17 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { DashboardModule } from './dashboard/dashboard.module';
+import { FormularioColetaComponent } from './dashboard/formulario/formulario-coleta/formulario-coleta.component';
+import { FormularioEclosaoComponent } from './dashboard/formulario/formulario-eclosao/formulario-eclosao.component';
+import { FormularioSolturaComponent } from './dashboard/formulario/formulario-soltura/formulario-soltura.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FormularioColetaComponent,
+    FormularioEclosaoComponent,
+    FormularioSolturaComponent
   ],
   imports: [
     BrowserModule,
