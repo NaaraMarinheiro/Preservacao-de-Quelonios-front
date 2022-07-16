52a3755a16c3597944801f0a59984d7ee819fee9
Naara Marinheiro <naara.adv@gmail.com>
2022-07-16 01:57:07 -0300

    ajustes de padronizacao 3

---
+++ src/app/dashboard/home/home-coordenador/home-coordenador.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-home-coordenador',
+  templateUrl: './home-coordenador.component.html',
+  styleUrls: ['./home-coordenador.component.css']
+})
+export class HomeCoordenadorComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
---
+++ src/app/dashboard/home/home-voluntario/home-voluntario.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-home-voluntario',
+  templateUrl: './home-voluntario.component.html',
+  styleUrls: ['./home-voluntario.component.css']
+})
+export class HomeVoluntarioComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
