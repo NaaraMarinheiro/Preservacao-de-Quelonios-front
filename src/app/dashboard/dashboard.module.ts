e4e2ce43b4de2bde9490051b49c06ec589198693
YacciRocha <yaccirs@gmail.com>
2022-07-21 20:16:22 -0300

    configuracao da pagina novo ciclo

--- src/app/dashboard/dashboard.module.ts
+++ src/app/dashboard/dashboard.module.ts
@@ -1,13 +1,16 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { ReactiveFormsModule } from '@angular/forms';
+
 import { DashboardRoutingModule } from './dashboard-routing.module';
-import { DashboardComponent } from './dashboard.component';
 import { CicloRoutingModule } from './ciclo/ciclo-routing.module';
 import { CicloModule } from './ciclo/ciclo.module';
 import { UsuarioModule } from './usuario/usuario.module';
 import { UsuarioRoutingModule } from './usuario/usuario-routing.module';
 import { RelatorioModule } from './relatorio/relatorio.module';
 import { RelatorioRoutingModule } from './relatorio/relatorio-routing.module';
+
+import { DashboardComponent } from './dashboard.component';
 import { HomeComponent } from './home/home.component';
 import { ViagemItemComponent } from './viagem/viagem-item/viagem-item.component';
 
@@ -27,6 +30,7 @@ import { ViagemItemComponent } from './viagem/viagem-item/viagem-item.component'
     UsuarioRoutingModule,
     RelatorioModule,
     RelatorioRoutingModule,
+    ReactiveFormsModule
   
     
 
