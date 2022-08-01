7a3f4e662ae6b5fcb883d57f1de26eaa633608a9
Pam-Guerra <pamelatguerra@gmail.com>
2022-08-01 11:43:08 -0300

    Criacao das Fetch e Post dos Formularios(Coleta, Eclosao e Soltura)

--- src/app/service/eclosao.service.ts
+++ src/app/service/eclosao.service.ts
@@ -1,9 +1,24 @@
 import { Injectable } from '@angular/core';
+import axios from 'axios';
 
 @Injectable({
   providedIn: 'root'
 })
 export class EclosaoService {
 
+  private urlEclosao = "http://localhost:8080/api/eclosao"
+
   constructor() { }
+
+  listAll() { }
+
+  async listByIDEclosao(idCiclo:string, idViagem:string, idEclosao:string) {
+    let result = await axios.get(this.urlEclosao + '/' + idEclosao);
+    return result.data;
+  }
+
+  async insert(novaEclosao: any) {
+    let result = await axios.post(this.urlEclosao, novaEclosao);
+    return result.status;
+  }
 }
